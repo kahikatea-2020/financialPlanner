04b1ac96a518d0566c5052ad8aa5886b57221d80
albasychris <albasychris@gmail.com>
2020-09-27 12:07:44 +1300

    complete backend of authentication

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './Home/Home'
 import Navbar from './Navbar'
+import Login from './Login'
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Login />
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,64 @@
-const server = require('./server')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
-const port = process.env.PORT || 3000
 
-server.listen(port, function () {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', port)
-})
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.set('debug', true);
+
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.stack,
+        error: err
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
