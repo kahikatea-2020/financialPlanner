e199e7df18bbf89de30453bdfa4dec214d2d0d52
albasychris <albasychris@gmail.com>
2020-09-27 12:50:14 +1300

    remove authentication

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require("body-parser");
-router.use(bodyParser.json());
-
-router.use('/users', require('./users'));
-
-module.exports = router;
