c3d905fe196f3fd35ccc15730e5478875b11a366
albasychris <albasychris@gmail.com>
2020-09-27 16:51:01 +1300

    implement react router dom

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Home from './Home/Home'
 import Navbar from './Navbar'
 import Login from './Login'
+import {Route} from 'react-router-dom'
 const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
-      <Login />
+      <Route path='/' exact component = {Home}/>
+      <Route path='/login' exact component = {Login}/>
     </>
   )
 }
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 class NavbarComponent extends Component {
   render() {
@@ -9,19 +10,12 @@ class NavbarComponent extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav className="ml-auto">
-            <Button style={{marginRight:"10px"}} variant="dark">Login</Button>
-            <Button variant="outline-dark">Sign Up</Button>
+            <Link to='/login'><Button style={{marginRight:"10px"}} variant="dark">Login</Button></Link>
+            <Link to='/signup'><Button variant="outline-dark">Sign Up</Button></Link>
           </Nav>
 
         </Navbar.Collapse>
