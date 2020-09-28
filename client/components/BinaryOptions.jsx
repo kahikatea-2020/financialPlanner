9af0063263cf85143b510e69a88e4511d45630db
albasychris <albasychris@gmail.com>
2020-09-29 11:47:00 +1300

    add a fixed value exposed balance for calculator

--- client/components/BinaryOptions.jsx
+++ client/components/BinaryOptions.jsx
@@ -13,7 +13,8 @@ class BinaryOptions extends Component {
     editRP: false,
     editIA: false,
     editEB: false,
-    finalCount: 0
+    finalCount: 0,
+    exposedAsPercent: true
   }
 
   calculate = async () => {
@@ -23,7 +24,7 @@ class BinaryOptions extends Component {
     } else {
       await this.setState({currentAmount: this.state.initialAmount})
       while (this.state.currentAmount < this.state.targetAmount) {
-        await this.setState({ currentAmount: Number(this.state.currentAmount) + (Number(this.state.exposedBalance) / 100) * Number(this.state.currentAmount) * (Number(this.state.rewardPercent) / 100) })
+        await this.setState({ currentAmount: Number(this.state.currentAmount) + (this.state.exposedAsPercent ? (Number(this.state.exposedBalance) / 100)* Number(this.state.currentAmount) : Number(this.state.exposedBalance))  * (Number(this.state.rewardPercent) / 100) })
         count++
         console.log(count)
         console.log(this.state.currentAmount)
@@ -46,7 +47,7 @@ class BinaryOptions extends Component {
             <p>Target Amount</p>
             {this.state.editTA ? <input type="number" placeholder="target amount" onChange={(evt) => {
               this.setState({ targetAmount: evt.target.value })
-            }} /> : this.state.targetAmount}
+            }} /> : "$" + this.state.targetAmount}
           </div>
           <div className="calculatorInput">
             {this.state.editRP ? <button onClick={() => this.setState({ editRP: false })}>submit</button> : <button onClick={() => this.setState({ editRP: true })}>edit</button>}
@@ -60,14 +61,15 @@ class BinaryOptions extends Component {
             <p>Initial Amount</p>
             {this.state.editIA ? <input type="number" placeholder="initial amount" onChange={(evt) => {
               this.setState({ initialAmount: evt.target.value })
-            }} /> : this.state.initialAmount}
+            }} /> : "$" + this.state.initialAmount}
           </div>
           <div className="calculatorInput">
             {this.state.editEB ? <button onClick={() => this.setState({ editEB: false })}>submit</button> : <button onClick={() => this.setState({ editEB: true })}>edit</button>}
+            {this.state.exposedAsPercent ? <button onClick={()=> this.setState({exposedAsPercent: false})}>Expose as $</button> : <button onClick={()=> this.setState({exposedAsPercent: true})}>Expose as %</button>}
             <p>Exposed Balance</p>
             {this.state.editEB ? <input type="number" placeholder="exposed balance" onChange={(evt) => {
               this.setState({ exposedBalance: evt.target.value })
-            }} /> : this.state.exposedBalance + "%"}
+            }} /> : this.state.exposedAsPercent? this.state.exposedBalance + "%" : "$" + this.state.exposedBalance}
           </div>
           <Button variant='dark' onClick={() => this.calculate()}>Calculate Amount of Remaining Successful Trades</Button>
           <h2>{this.state.finalCount + ' '} Sucessful Trades Remaining </h2>
