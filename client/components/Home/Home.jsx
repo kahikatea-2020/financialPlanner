2142dbd56ae52d668093a19a3ee1ffb490e63b01
albasychris <albasychris@gmail.com>
2020-09-30 18:59:18 +1300

    change home page layout

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -1,12 +1,56 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './home.css'
-class Home extends Component{
-  render(){
+import { Container, Carousel, Row, Col } from 'react-bootstrap'
+
+class Home extends Component {
+  render() {
     return <>
-    <div className="homeCarousel">
-       <h1>Manage Your Finances Here</h1>
-    </div>
-      <h1>Welcome Home</h1>
+      <Carousel className="homeCarousel" interval='5000'>
+        <Carousel.Item>
+
+
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
+        </Carousel.Item>
+        <Carousel.Item>
+
+
+
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+        </Carousel.Item>
+        <Carousel.Item>
+
+
+
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+
+        </Carousel.Item>
+      </Carousel>
+      <Container className="homeDescriptions"style={{ marginTop: '50px' }}>
+        <h1>Why Us?</h1>
+        <Row>
+          <Col className="columnHome" lg={4}>
+            <i class="fab fa-react"></i>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptate vero, odit consequuntur quod illo ut quidem qui ex vel! Necessitatibus, nemo autem. Perspiciatis fugiat provident id tenetur modi eos sit delectus, est maxime, odit reiciendis laudantium magni officia blanditiis placeat commodi voluptatem vitae architecto rerum itaque </p>
+          </Col>
+          <Col className="columnHome" lg={4}>
+            <i class="fab fa-react"></i>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptate vero, odit consequuntur quod illo ut quidem qui ex vel! Necessitatibus, nemo autem. Perspiciatis fugiat provident id tenetur modi eos sit delectus, est maxime, odit reiciendis laudantium magni officia blanditiis placeat commodi voluptatem vitae architecto rerum itaque </p>
+          </Col>
+          <Col className="columnHome" lg={4}>
+            <i class="fab fa-react"></i>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptate vero, odit consequuntur quod illo ut quidem qui ex vel! Necessitatibus, nemo autem. Perspiciatis fugiat provident id tenetur modi eos sit delectus, est maxime, odit reiciendis laudantium magni officia blanditiis placeat commodi voluptatem vitae architecto rerum itaque </p>
+          </Col>
+        </Row>
+
+
+
+      </Container>
+
     </>
   }
 }
