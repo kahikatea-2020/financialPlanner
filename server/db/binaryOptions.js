ca27aedfadc32ec7e678368d2d9c1b3c3e2b4445
albasychris <albasychris@gmail.com>
2020-10-06 20:00:17 +1300

    write getUserOptions and updateUserOption

--- server/db/binaryOptions.js
+++ server/db/binaryOptions.js
@@ -1,7 +1,9 @@
 const connection = require('./connection')
 
 module.exports = {
-  createOption
+  createOption,
+  getUserOptions,
+  updateUserOption
 }
 
 function createOption (option, db = connection) {
@@ -15,4 +17,24 @@ function createOption (option, db = connection) {
     history: option.history,
     win_rate: option.winRate
   })
+}
+
+function getUserOptions (user_id, db = connection) {
+  db('binaryOptions')
+    .select()
+    .where('user_id',user_id)
+}
+
+function updateUserOption (option , db = connection) {
+  db('binaryOptions')
+    .update({
+      id: option.id,
+    user_id: option.userId,
+    target_amount: option.targetAmount,
+    reward_percent: option.rewardPercent,
+    initial_amount: option.initialAmount,
+    exposed_balance: option.exposedBalance,
+    history: option.history,
+    win_rate: option.winRate
+    })
 }
