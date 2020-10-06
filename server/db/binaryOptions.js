5a31cef00e7f2da72377dadaff75bb9cad6dd34b
albasychris <albasychris@gmail.com>
2020-10-06 20:06:20 +1300

    write deleteUserOption function

--- server/db/binaryOptions.js
+++ server/db/binaryOptions.js
@@ -4,26 +4,45 @@ const snakeCaseKeys = require('snakecase-keys')
 module.exports = {
   createOption,
   getUserOptions,
-  updateUserOption
+  updateUserOption,
+  deleteUserOption
 }
 
-function createOption (option, db = connection) {
+function createOption(option, db = connection) {
   db('binaryOptions').insert(snakeCaseKeys(option))
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
 }
 
-function getUserOptions (user_id, db = connection) {
+function getUserOptions(user_id, db = connection) {
   db('binaryOptions')
     .select()
-    .where('user_id',user_id)
+    .where('user_id', user_id)
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
 }
 
-function updateUserOption (optionId, option , db = connection) {
+function updateUserOption(optionId, option, db = connection) {
   db('binaryOptions')
-  .where('id', optionId)
-  .update(snakeCaseKeys(option))
-  .then(() => db('expense').where('id', expenseId).select().first())
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.error(err)
-  })
+    .where('id', optionId)
+    .update(snakeCaseKeys(option))
+    .then(() => db('expense').where('id', expenseId).select().first())
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
+}
+
+function deleteUserOption(optionId, db = connection) {
+  db('binaryOptions')
+    .where('id', optionId)
+    .delete()
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
 }
