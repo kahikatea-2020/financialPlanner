e125b41c0eaf9855c24e3c31c56bcc8096d11ec1
albasychris <albasychris@gmail.com>
2020-10-07 13:38:05 +1300

    add profiles seed

--- seeds/users.js
+++ seeds/users.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcrypt')
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
+  let password = await bcrypt.hash('123456',10)
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, email:'proteus@gmail.com', password: bcrypt.hash('123456',10)},
+        {id: 1, email:'proteus@gmail.com', password: password},
       ]);
     });
 };
