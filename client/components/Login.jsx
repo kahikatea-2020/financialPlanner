ec1b00f3d38af94e63498487a17bb3a74adf2f2b
albasychris <albasychris@gmail.com>
2020-10-17 12:24:12 +1300

    complete login page styling

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { authUser } from '../store/actions/auth'
+import {Button} from 'react-bootstrap'
+import './register.css'
 class Logins extends Component {
 
   state = {
@@ -10,7 +12,7 @@ class Logins extends Component {
   }
   handleClick = async () => {
 
-     try {
+    try {
       await this.props.authUser({ email: this.state.email, password: this.state.password })
       location.replace("/#/profile")
     } catch (err) {
@@ -20,19 +22,31 @@ class Logins extends Component {
 
   render() {
     return (
-      <div>
-        <input type="email" placeholder="email" onChange={(evt) => {
+      <div className="registerContainer">
+        <div>
+          <h1>Log In</h1>
+          <hr/>
+          <div className="inputContainer">
+            <input type="email" placeholder="email" onChange={(evt) => {
 
-          this.setState({ email: evt.target.value })
+              this.setState({ email: evt.target.value })
 
-        }} />
-        <input type="password" placeholder="password" onChange={(evt) => {
+            }} />
+          </div>
+          <div className="inputContainer">
+            <input type="password" placeholder="password" onChange={(evt) => {
 
-          this.setState({ password: evt.target.value })
+              this.setState({ password: evt.target.value })
 
-        }} />
-        <button onClick={() => this.handleClick()}>Log In</button>
-        <p>{this.state.err && this.state.err.message}</p>
+            }} />
+          </div>
+
+
+          <Button variant = "outline-light" style={{fontSize:"1.5rem", borderRadius:"10px",paddingLeft:"50px", paddingRight: "50px", marginTop: "50px"}} onClick={() => this.handleClick()}>Log In</Button>
+          <p>{this.state.err && this.state.err.message}</p>
+        </div>
+
+
       </div>
     )
   }
