c8e740f0dfc093f10b9a8be4e1d9b21f4f41ec9a
albasychris <albasychris@gmail.com>
2020-10-18 12:23:49 +1300

    undo error changes

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -49,7 +49,7 @@ class Register extends Component {
                 location.replace('/#/profile')
 
               } catch (err) {
-                this.setState({ err: err.stack })
+                this.setState({ err: err.message })
               }
             }}>Sign Up</Button>
           </form>
